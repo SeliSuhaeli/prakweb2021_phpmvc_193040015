c52b1dd6a67dca3414d7eff54b9832211ad22efc
SeliSeptian <selisuhaeliseptian25@gmail.com>
2021-09-30 09:05:07 +0700

    Menyelesaikan latihan di video ke-4 membuat Controller

---
+++ app/controllers/About.php
@@ -0,0 +1,13 @@
+<?php
+
+class About
+{
+  public function index($nama = ' Sandhika', $pekerjaan = 'Dosen')
+  {
+    echo "Hallo, nama saya $nama, saya adalah sorang $pekerjaan";
+  }
+  public function page()
+  {
+    echo 'About/page';
+  }
+}
